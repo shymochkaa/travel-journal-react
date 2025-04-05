ba30bfeac7d63899788bd0a390857ee9be17bb38
Hanna Shymanovych <shimsnov111@gmail.com>
2025-04-05 23:00:00 +0200

    Complete project with final functionality and design

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <header>
+            <img className="app-logo" src="./assets/globe.png" alt="globe icon"/>
+            <a className="logo-link" href="#">my travel journal.</a>
+        </header>
+    )
+}
+
